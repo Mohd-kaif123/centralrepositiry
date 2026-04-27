62b1eb641402b66011d40c67647791709775eb2f
Mohd-kaif123 <mansoorikaif365@gmail.com>
2026-04-27 14:38:20 +0000

    class practise

--- python/class.py
+++ python/class.py
@@ -1,3 +1,29 @@
-class server:
+'''class server:
     def __init__(self, name,ip):
-        
+        self.name = name
+        self.ip = ip
+    def status(self):
+        print(f"{self.name} at {self.ip} is running")
+
+web_server = server("Webserver","198.162.1.10")
+web_server.status()'''
+
+
+
+
+
+
+
+
+
+class server:
+    def __init__(self,name,ip,subnet):
+        self.name=name
+        self.ip=ip
+        self.subnet=subnet
+    
+    def curent_status(self):
+        print(f"{self.name} is running.....")
+        print(f"{self.name} IP Address and subnet is {self.ip}\n{self.subnet}")
+webserver=server("webserver","198.162.121","255.255.255.0")    
+webserver.curent_status()
