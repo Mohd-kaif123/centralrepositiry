599bf19de3c3523587077d042cce2f25bcaa8a3e
Mohd-kaif123 <mansoorikaif365@gmail.com>
2026-04-28 17:23:06 +0000

    Another Practise for class

--- python/class.py
+++ python/class.py
@@ -1,3 +1,4 @@
+# yeha print karo Webserver at 198.162.1.10 is running
 '''class server:
     def __init__(self, name,ip):
         self.name = name
@@ -8,15 +9,8 @@
 web_server = server("Webserver","198.162.1.10")
 web_server.status()'''
 
-
-
-
-
-
-
-
-
-class server:
+# Yeha per bhi wahi print karo but with subnet mask
+'''class server:
     def __init__(self,name,ip,subnet):
         self.name=name
         self.ip=ip
@@ -26,4 +20,42 @@ class server:
         print(f"{self.name} is running.....")
         print(f"{self.name} IP Address and subnet is {self.ip}\n{self.subnet}")
 webserver=server("webserver","198.162.121","255.255.255.0")    
-webserver.curent_status()
+webserver.curent_status()'''
+
+# print karna hai name roll no. aur marks
+'''class student:
+    def __init__(self,name,roll_no,marks):
+        self.name=name
+        self.roll_no=roll_no
+        self.marks=marks
+    def introduction(self):
+        print(f"mera naam {self.name} hai, mera roll no. {self.roll_no}")
+        if self.marks>=40:
+            print("Pass")
+        else:
+            print("Fail")
+
+Details=student("kaif",21,45)
+Details.introduction()'''
+
+
+# 3 alag alg car bano
+
+class car:
+    def __init__(self,brand,color,speed):
+        self.brand=brand
+        self.color=color
+        self.speed=speed
+
+    def details(self):
+        print(f"brand name hai {self.brand}")
+        print(f"car ka color hai {self.color}")
+        print(f"car ki speed hai {self.speed}")
+    
+    def honk(self):
+        print(f"{self.brand} -- Beep Beep!")
+
+cars_detail=car("bukati","Black","280km/hr")
+cars_detail.details()
+cars_detail.honk()
+
