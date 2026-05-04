9675a6d6abce1f0b50ab9f558c790ecee8336aeb
Mohd-kaif123 <mansoorikaif365@gmail.com>
2026-05-04 09:52:22 +0000

    practise related to inheritance

---
+++ python/p1.py
@@ -0,0 +1,45 @@
+class bankaccount:
+    def __init__(self,owner,balance):
+        self.owner=owner
+        self.balance=balance
+    def deposite(self,amount):
+        self.balance+=amount
+        print(f"{amount} Deposite successfully. New Balance is : {self.balance}")
+    def withdraw(self,w_amount):
+        self.w_amount=w_amount
+        if self.balance>w_amount:
+            self.balance-=w_amount            
+            print(f"Withdraw successfully : {w_amount} ")
+        else:
+            print("Balance is insufficient")
+    def c_balance(self):
+        print(f"Available Balance: {self.balance}")
+
+class s_account(bankaccount):
+    def interest_add(self):
+        self.interest_add=self.balance*0.10
+        self.balance+=self.interest_add
+        print(f"Balance with Interest: {self.balance}")
+class c_account(bankaccount):
+    def overdruft(self, limit):
+        self.limit=limit
+        print(f"Over draft limit set to : {self.limit}")
+    def withdraw(self,W_amount):
+        self.W_amount=W_amount
+        if self.W_amount<self.limit:
+            self.balance-=self.W_amount
+            print(f"Amount Withdraw sucessfully: {self.W_amount}")
+        else:
+            print("Anount is over to limit")
+
+s1=s_account("Kaif",50000)
+s1.deposite(30000)
+s1.withdraw(60000)
+s1.interest_add()
+s1.c_balance()
+
+c1=c_account("Wais",100000)
+c1.deposite(50000)
+c1.overdruft(100000)
+c1.withdraw(60000)
+c1.c_balance()
