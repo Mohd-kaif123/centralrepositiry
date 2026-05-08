a9fc7dc06e84b004c95ae0e7d9a947603b7fe366
Mohd-kaif123 <mansoorikaif365@gmail.com>
2026-05-08 10:40:26 +0000

    Os module practise and concept

--- python/polymorphism2.py
+++ python/polymorphism2.py
@@ -15,4 +15,34 @@ for work in working:
     work.kaam_karo()
 
 
-#______________LEVEL-2 Q2)______________#
+#______________LEVEL-3 Q1)______________#
+
+
+class warrior:
+    def attack(self):
+        print("Attack by sword!")
+    def defend(self):
+        print("Shield de defence kar raha hai!")
+
+class Archer:
+    def attack(self):
+        print("Teer se attack kar rahi hai")
+    def defend(self):
+        print("Shield se protect hai!")
+
+class wizarrd:
+    def attack(self):
+        print("Attack throug fire ball")
+    def defend(self):
+        print("Defend through Invesible shield")
+
+class Healer:
+    def attack(self):
+        print("Healing other Team member")
+    def defend(self):
+        print("special power defend kar rahi hai")
+
+clash=[warrior(), Archer(), wizarrd(), Healer()]
+for clans in clash:
+    clans.attack()
+    clans.defend()
