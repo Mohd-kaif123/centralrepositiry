1697cfc09979e50a573e973f81473bd871d0fb0d
Mohd-kaif123 <mansoorikaif365@gmail.com>
2026-06-26 17:31:01 +0000

    Numpy practise

---
+++ python/Python_modules/numpypract.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+'''
+arr=np.array([10, 20, 30, 40, 50])
+print(arr)
+print(arr.sum())
+'''
+'''
+zeros = np.zeros(5)
+
+# Ab isko print kar do
+print(zeros)
+'''
+'''
+e_array=np.arange(2,21, 2)
+print(e_array)
+'''
+
+'''
+# 1. 1 se 12 tak ka array banao
+array = np.arange(1, 13)
+
+# 2. .reshape() ka use karke use 3x4 matrix me badlo
+matrix= array.reshape(3, 4)
+
+# 3. Ab print kar do
+print(matrix)
+'''
+'''
+arr=np.max([25, 40, 15, 80, 65])
+print(arr)
+'''
+'''
+arr=np.min([25, 40, 15, 80, 65])
+print(arr)
+
+arr1=np.mean([25, 40, 15, 80, 65])
+print(arr1)
+'''
+'''
+arr=np.array([10, 20, 30, 40, 50])
+u_array=arr+5
+print(u_array)
+'''
+
