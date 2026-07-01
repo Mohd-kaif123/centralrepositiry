725809e8e8ef0b00992c7cf863357b885e343a51
Mohd-kaif123 <mansoorikaif365@gmail.com>
2026-07-01 10:32:35 +0000

    logging module practise

--- Python2/logging/logingpract.py
+++ Python2/logging/logingpract.py
@@ -1,5 +1,42 @@
 import logging
 
+'''
 logging.basicConfig(
-    filename='app.log'
+    filename='system.log',
+    level=logging.DEBUG,
+    format='%(asctime)s - %(levelname)s - %(message)s'
 )
+logging.info('root:Application Started')
+'''
+'''
+logging.basicConfig(
+    filename='system.log',
+    level=logging.DEBUG,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+logging.info('Root: Application started')
+logging.info('Root: Loading Data')
+logging.info('Root: Program Finished')
+logging.warning('root: Disk usage is High')
+logging.error('root: Application Crashed')
+'''
+'''
+logging.basicConfig(
+    filename='system.log',
+    level=logging.DEBUG,
+    format='%(levelname)s:%(message)s:%(name)s'
+)
+name =input("Enter Name:")
+logging.info(f"User {name} Logged IN ")
+'''
+'''
+logging.basicConfig(
+    filename='system.log',
+    level=logging.DEBUG,
+   format='%(asctime)s - %(levelname)s - %(message)s'
+)
+def backup():
+    logging.info("Backup  started")
+    logging.info("Backup completed")
+backup()
+'''
